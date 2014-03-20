b2335ecb7c9b84f6a3c738d35baff1d18842e467
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-03-20 16:51:29 +0100

    Add support for Clover

--- transmart-core-db-tests/grails-app/conf/BuildConfig.groovy
+++ transmart-core-db-tests/grails-app/conf/BuildConfig.groovy
@@ -16,6 +16,27 @@ grails.project.fork = [
         console: defaultVMSettings
 ]
 
+def enableClover = System.getenv('CLOVER')
+
+if (enableClover) {
+    grails.project.fork.test = false
+
+    clover {
+        on = true
+
+        def dirs = ['src/java', 'src/groovy', 'test/unit',
+                    'test/integration', 'grails-app']
+        srcDirs = dirs.inject dirs, { accum, cur ->
+            File dir = new File('..', cur)
+            if (dir.exists()) {
+                accum + dir.canonicalPath
+            } else {
+                accum
+            }
+        }
+    }
+}
+
 grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
     log "warn"
@@ -31,6 +52,12 @@ grails.project.dependency.resolution = {
         compile('org.hamcrest:hamcrest-library:1.3',
                 'org.hamcrest:hamcrest-core:1.3')
         compile 'com.h2database:h2:1.3.175'
+
+        if (enableClover) {
+            compile 'com.cenqua.clover:clover:3.2.2', {
+                export = false
+            }
+        }
 
         test('junit:junit:4.11') {
             transitive = false /* don't bring hamcrest */
@@ -55,6 +82,12 @@ grails.project.dependency.resolution = {
         build ':tomcat:7.0.47'
         build ':release:3.0.1', ':rest-client-builder:2.0.1', {
             export = false
+        }
+
+        if (enableClover) {
+            compile 'org.grails.plugins:clover:3.2.2', {
+                export = false
+            }
         }
     }
 }
